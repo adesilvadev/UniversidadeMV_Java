34509562f5be2884424ffe4f7f56994dfe2369e0
adesilvadev <adesilva.dev@gmail.com>
2023-02-20 18:06:06 -0300

    Entrega do exercício 2.

--- src/edu/java/aula4/Exercicio2.java
+++ src/edu/java/aula4/Exercicio2.java
@@ -1,9 +1,26 @@
 package edu.java.aula4;
 
+import java.util.Scanner;
+
 public class Exercicio2 {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        final double pi = 3.14;
+
+        double raio;
+        raio = sc.nextDouble();
+
+        double resultado = pi * (raio * raio);
+        System.out.printf("%.4f", resultado);
+
+        System.out.println();
+
+
+
+        sc.close();
 
 
     }
---
+++ src/edu/java/aula4/Exercicio3.java
@@ -0,0 +1,10 @@
+package edu.java.aula4;
+
+public class Exercicio3 {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
