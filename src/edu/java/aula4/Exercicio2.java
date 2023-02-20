ccdd40f971ece74488a50777a7cb3625284b1245
adesilvadev <adesilva.dev@gmail.com>
2023-02-20 18:27:39 -0300

    Correção dos exercícios 1, 2 e 3.

--- src/edu/java/aula4/Exercicio2.java
+++ src/edu/java/aula4/Exercicio2.java
@@ -1,14 +1,16 @@
 package edu.java.aula4;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Exercicio2 {
 
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        final double pi = 3.14;
+        final double pi = 3.14159;
 
         double raio;
         raio = sc.nextDouble();
