fbf33172102db0409238bd0e00d37986c2c60a38
adesilvadev <adesilva.dev@gmail.com>
2023-02-20 18:14:11 -0300

    Entrega do exercício 3.

--- src/edu/java/aula4/Exercicio3.java
+++ src/edu/java/aula4/Exercicio3.java
@@ -1,10 +1,32 @@
 package edu.java.aula4;
 
+import java.util.Scanner;
+
 public class Exercicio3 {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
+        int a;
+        a = sc.nextInt();
+
+        int b;
+        b = sc.nextInt();
+
+        int c;
+        c = sc.nextInt();
+
+        int d;
+        d = sc.nextInt();
+
+        int conjunto1 = a*b;
+        int conjunto2 = c*d;
+
+        int diferenca = conjunto1 - conjunto2;
+        System.out.println("O resultado do calculo da diferenca e igual a:"+ " " + diferenca);
+
+        sc.close();
 
     }
 }
