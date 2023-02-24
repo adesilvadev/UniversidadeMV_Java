d5226e44b2e9912099ca14cdc7cb502060f60fc2
adesilvadev <adesilva.dev@gmail.com>
2023-02-24 18:04:35 -0300

    Testes das questões de provas. Acertei tudo e tirei 8... Vou perturbar a vida do suporte. Rsrsrs

---
+++ src/edu/java/provas/Prova1.java
@@ -0,0 +1,20 @@
+package edu.java.provas;
+
+import java.util.Scanner;
+
+public class Prova1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x,y;
+        x = sc.nextInt();
+        y = x + 2;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        sc.close();
+    }
+}
