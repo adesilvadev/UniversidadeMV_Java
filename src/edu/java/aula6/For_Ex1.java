67a56625b807b853f1690d7195c9361c74ed7a8c
adesilvadev <adesilva.dev@gmail.com>
2023-02-24 15:24:13 -0300

    Estruturas de repetição - While, For e Do-While 3 exemplos

--- src/edu/java/aula6/For_Ex1.java
+++ src/edu/java/aula6/For_Ex1.java
@@ -1,4 +1,16 @@
 package edu.java.aula6;
 
+import java.util.Scanner;
+
 public class For_Ex1 {
+
+    public static void main(String[] args) {
+
+        // Regra: ( tipo | inicio | Condição | Incremento/Decremento)
+        for (int i=4; i>0; i--){
+
+            System.out.println("Valor de i: "+ i);
+        }
+
+    }
 }
--- src/edu/java/aula6/While_Ex1.java
+++ src/edu/java/aula6/While_Ex1.java
@@ -1,4 +1,26 @@
 package edu.java.aula6;
 
+import java.util.Scanner;
+
 public class While_Ex1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int valor = sc.nextInt();
+
+        while (valor < 5) {
+
+            System.out.println("Aqui:"+ " " + valor);
+            break;
+        }
+
+        if (valor > 5) {
+            System.out.println("O numero informado é maior que 5.");
+            System.out.println("Fim do programa. Obrigado!");
+        }
+
+        sc.close();
+    }
 }
